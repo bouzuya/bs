ded5d226bf19548466874861b99bf547b154ba90
bouzuya <m@bouzuya.net>
2018-05-31 20:13:25 +0900

    show current directory files

--- src/Main.purs
+++ src/Main.purs
@@ -1,9 +1,24 @@
 module Main where
 
-import Prelude
 import Control.Monad.Eff (Eff)
 import Control.Monad.Eff.Console (CONSOLE, log)
+import Control.Monad.Eff.Exception (EXCEPTION)
+import Node.FS (FS)
+import Node.FS.Sync (readdir)
+import Prelude (Unit, bind, discard, show)
 
-main :: forall e. Eff (console :: CONSOLE | e) Unit
+main
+  :: forall e
+  . Eff
+    ( console :: CONSOLE
+    , exception :: EXCEPTION
+    , fs :: FS
+    | e
+    )
+    Unit
 main = do
+  let
+    currentDirectory = "."
+  files <- readdir currentDirectory
+  log (show files)
   log "Hello sailor!"
