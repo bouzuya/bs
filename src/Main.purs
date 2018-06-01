95d887a139f6cd582b44a6392ebc691faf0755cb
bouzuya <m@bouzuya.net>
2018-06-01 21:22:22 +0900

    list dirs

--- src/Main.purs
+++ src/Main.purs
@@ -4,10 +4,14 @@ import Control.Monad.Eff (Eff)
 import Control.Monad.Eff.Console (CONSOLE, log)
 import Control.Monad.Eff.Exception (EXCEPTION)
 import Data.Array as Array
+import Data.Maybe (Maybe(..))
+import Data.Traversable (for)
 import Node.FS (FS)
-import Node.FS.Sync (readdir)
+import Node.FS.Stats (isDirectory)
+import Node.FS.Sync (readdir, stat)
 import Node.Path (extname)
-import Prelude (Unit, bind, compose, discard, eq, map, show, (<<<))
+import Node.Path as Path
+import Prelude (Unit, bind, compare, compose, discard, eq, flip, map, pure, show)
 
 main
   :: forall e
@@ -20,11 +24,24 @@ main
     Unit
 main = do
   let
-    currentDirectory = "."
-  files <- readdir currentDirectory
+    dir = "./src"
+  files <- map (map (compose Path.concat (Array.snoc [dir]))) (readdir dir)
   let
     filter = Array.filter (compose (eq ".json") extname)
-    filteredFiles = filter files
-    lastFilteredFile = compose Array.last Array.sort filteredFiles
-  log (show lastFilteredFile)
+    sortedFiles = Array.sortBy (flip compare) files -- order by desc
+    filteredFiles = filter sortedFiles
+    lastFilteredFile = Array.head filteredFiles
+  file <- case lastFilteredFile of
+    Just f -> pure f
+    Nothing -> do
+      maybes <- for sortedFiles \f -> do
+        s <- stat f
+        pure if isDirectory s
+          then Just f
+          else Nothing
+      let
+        dirs = Array.catMaybes maybes
+      log (show dirs)
+      pure ""
+  log (show file)
   log "Hello sailor!"
